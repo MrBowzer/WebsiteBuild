02f440e92146d9d276f9b7b0dc758de5c5000d0f
Samir Bawezir <samirbawezir@gmail.com>
2016-08-02 23:46:38 +0000

    start working on the site, trying to get images on though

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <title>Website Build</title>
-        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--link to bootstrap-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
         <style>
-            .navbar{
-                margin-bottom:0px;
-            }
+
+            
             
         </style>
 
@@ -16,12 +17,12 @@
     
     <body>
         <!--NAV Begins-->
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
                 <div class=navbar-header>
                     <a class="navbar-brand" href="#">Web Builder</a>
                 </div>
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav  ">
                     <li><a href="#">What its about</a></li>
                     <li><a href="#">what it does</a></li>
                     <li><a href="#">why i use it</a></li>
@@ -29,9 +30,26 @@
             </div>
         </nav>
         <!--NAV Ends-->
-        <div class="jumbotron">
-            
+        
+        <!--jumbotron begins-->
+        <div class="jumbotron text-center">
+            <h1>Welcome to the Website</h1>
+            <p>Where Im going to learn how to bulid a site step by step</p>
         </div>
+        <!--jumbotron ends-->
+        
+        <!--About me begins-->
+        <div class="container-fluid">
+            <div class="row">
+                <h2 class="text-center">A little about Myself<h2>
+            </div>
+            <div class="row">
+                <div class="col-xs-4"><p class="text-center">I is smart</p></div>
+                <div class="col-xs-4"><p class="text-center">I is smart</p></div>
+                <div class="col-xs-4"><p class="text-center">I is smart</p></div>
+            </div>
+        </div>
+        <!--about me ends>
         
     </body>
 
