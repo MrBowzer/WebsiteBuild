ce145d35359e1ed52d6f81a0512c5adbe20969a0
Samir Bawezir <samirbawezir@gmail.com>
2016-08-02 21:42:49 +0000

    added bootstrap and a navbar

--- index.php
+++ index.php
@@ -2,12 +2,37 @@
 <html>
     <head>
         <title>Website Build</title>
+        
+        <!--link to bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style>
+            .navbar{
+                margin-bottom:0px;
+            }
+            
+        </style>
+
     </head>
     
     <body>
-        <h1>Hello World</h1>
-        <h2>This is my new websites</h2>
-    
+        <!--NAV Begins-->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class=navbar-header>
+                    <a class="navbar-brand" href="#">Web Builder</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="#">What its about</a></li>
+                    <li><a href="#">what it does</a></li>
+                    <li><a href="#">why i use it</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!--NAV Ends-->
+        <div class="jumbotron">
+            
+        </div>
+        
     </body>
 
 
