1302f552a571d0174b3e5b894d11865e4fe395a2
Samir Bawezir <samirbawezir@gmail.com>
2016-08-07 01:20:31 +0000

    started from scratch, changed the jumbotron picture

---
+++ holdmycode.php
@@ -0,0 +1,51 @@
+ <!--my story-->
+        <div class="container-fluid story">
+            <div class="row">
+                <div class="col-xs-9">
+                    <h3>Who am I </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
+                </div>  
+                <div class="col-xs-3">
+                    <img src="images/pro.jpg" class="img-responsive" width="300px" height="450px" >
+                </div>
+            </div>
+        </div>
+        <!--story ends-->
+            
+            
+         <!--About me begins-->
+        <div class="container-fluid text-center aboutme">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>A little about Myself</h2>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-xs-4 center-align">
+                    <p>This is me getting second in my districtLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
+                   <img src="images/district.jpg" class="img-responsive">
+                </div>
+                
+                 <div class="col-xs-4 center-align">
+                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
+                    <img src="images/district.jpg" class="img-responsive">
+                </div>
+                
+                 <div class="col-xs-4 center-align">
+                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
+                    <img src="images/district.jpg" class="img-responsive">
+                </div>
+                
+            </div>
+        </div>
+        <!--about me ends-->
+        
+        <!--footer begins-->
+            
+        <!--footer ends-->
+        
+        <div class="container-fluid xs-2 text-center">
+            
+        </div>
+        
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
           margin-bottom:0px;
           padding-top: 20%;
           padding-bottom:22%;
-          background-image: url("images/jumbotron.jpe");
+          background-image: url("images/jumbotron-1.jpg");
           color:#fff;
         }
         
@@ -21,54 +21,39 @@
             opacity:.91;
         }
         
-        .story {
-           background-color:#F7EF6A; 
-           padding-top: 40px;
-           padding-bottom: 60px;
-           padding-right: 100px;
-           padding-left: 100px;
-           font-size:20px;
-        }
-        
-        .h3,h3{
-            font-size:50px;
-        }
-        
-        .h2,h2{
-            font-size:40px;
-        }
-        
-        p{
-            font-size:20px;
-            
-        }
-        
-        .aboutme {
-            padding-top: 40px;
-            padding-bottom: 60px;
-            padding-left: 100px;
-            padding-right: 100px;
-            background-color:#465C8B;
-            color:#fff;
-            
-        }
-        
-        .center-align{
-            text-align:center;
-        }
-        
         .btn {
-            background-color:#93C178;
-            border-color:transparent;
-            
+            background-color:transparent;
+            border-color:#fff;
+            color:white;
         }
         
         .btn:hover{
-            background-color:yellow;
+            background-color:#93C178;
+            
         }
         
         .a:visited{
             background-color:#93C178;
+        }
+        
+        .margin{
+            margin-bottom:45px;
+        }
+        
+        .container-1{
+            padding-top: 45px;
+            padding-bottom:45px;
+            padding-left:60px;
+            padding-right:60px;
+            background-color:#DFDCE3;
+        }
+    
+        .h3, h3{
+            font-size:40px;
+        }
+        
+        p {
+            font-size:20px;
         }
         
             
@@ -90,57 +75,24 @@
         </div>
         <!--jumbotron ends-->
         
-       <!--my story-->
-        <div class="container-fluid story">
+        <div class="container-fluid text-center container-1">
+            <h3 class="margin">My Hobbies</h3>
             <div class="row">
-                <div class="col-xs-9">
-                    <h3>Who am I </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                </div>  
-                <div class="col-xs-3">
-                    <img src="images/pro.jpg" width="300" height="450">
+                <div class="col-md-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales maximus augue, accumsan viverra erat tristique non. Donec nec bibendum lacus</p>
+                    <img src="images/district.jpg" class="img-responsive margin" style="width:100%;">
+                </div>
+                <div class="col-md-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales maximus augue, accumsan viverra erat tristique non. Donec nec bibendum lacuss</p>
+                    <img src="images/district.jpg" class="img-responsive margin" style="width:100%;">
+                </div>
+                <div class="col-md-4">
+                    <p>I Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales maximus augue, accumsan viverra erat tristique non. Donec nec bibendum lacus</p>
+                    <img src="images/district.jpg" class="img-responsive margin" style="width:100%;">
                 </div>
             </div>
         </div>
-        <!--story ends-->
-            
-            
-         <!--About me begins-->
-        <div class="container-fluid text-center aboutme">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h2>A little about Myself</h2>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my districtLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                   <img src="images/district.jpg">
-                </div>
-                
-                 <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                    <img src="images/district.jpg">
-                </div>
-                
-                 <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                    <img src="images/district.jpg">
-                </div>
-                
-            </div>
-        </div>
-        <!--about me ends-->
-        
-        <!--footer begins-->
-            
-        <!--footer ends-->
-        
-        <div class="container-fluid xs-2 text-center">
-            
-        </div>
-        
+      
         
     </body>
 
