ce6f061f64fa6cb6468c568e04a8066fcc178479
Samir Bawezir <samirbawezir@gmail.com>
2016-08-08 03:06:35 +0000

    tried to fixed the collaspe for navbar, but cant get the js to work

--- holdmycode.php
+++ holdmycode.php
@@ -1,51 +1,23 @@
- <!--my story-->
-        <div class="container-fluid story">
-            <div class="row">
-                <div class="col-xs-9">
-                    <h3>Who am I </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                </div>  
-                <div class="col-xs-3">
-                    <img src="images/pro.jpg" class="img-responsive" width="300px" height="450px" >
-                </div>
-            </div>
-        </div>
-        <!--story ends-->
-            
-            
-         <!--About me begins-->
-        <div class="container-fluid text-center aboutme">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h2>A little about Myself</h2>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my districtLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                   <img src="images/district.jpg" class="img-responsive">
-                </div>
-                
-                 <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                    <img src="images/district.jpg" class="img-responsive">
-                </div>
-                
-                 <div class="col-xs-4 center-align">
-                    <p>This is me getting second in my district Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit quam ut diam tincidunt pulvinar. Proin condimentum interdum lorem facilisis tincidunt. Nulla et magna arcu. Sed sollicitudin, magna sit amet blandit vestibulum, nisi sem cursus lacus, vel tristique leo ligula eu lorem. Nulla facilisis elit lacus, placerat mattis nulla porttitor sit amet. Aenean id pellentesque est, quis ultricies lacus. Praesent sodales imperdiet orci sed aliquet. Sed dapibus mattis consectetur. In sit amet risus quis dolor egestas posuere.</p>
-                    <img src="images/district.jpg" class="img-responsive">
-                </div>
-                
-            </div>
-        </div>
-        <!--about me ends-->
-        
-        <!--footer begins-->
-            
-        <!--footer ends-->
-        
-        <div class="container-fluid xs-2 text-center">
-            
-        </div>
-        
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Simple Collapsible</h2>
+  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+  <div id="demo" class="collapse">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  </div>
+</div>
+
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--link to bootstrap-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
         <style>
         .navbar{
             margin-bottom:0px;
@@ -33,7 +35,8 @@
         }
         
         .btn:hover{
-            background-color:#93C178;
+            background-color:black;
+            color:#fff;
             
         }
         
